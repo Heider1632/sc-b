862b1aac9b6f179361aa7a805b1bd4edbc563d76
Heider Zapa <36663423+Heider1632@users.noreply.github.com>
2022-05-10 16:44:47 -0500

    update next assessment

--- src/routes/assessment.routes.js
+++ src/routes/assessment.routes.js
@@ -2,13 +2,13 @@ const { authJwt } = require("../middlewares");
 const AssessmentController = require("../controllers/assessment.controller");
 
 module.exports = function(app) {
-    app.use(function(req, res, next) {
-        res.header(
-            "Access-Control-Allow-Headers",
-            "x-access-token, Origin, Content-Type, Accept"
-        );
-        next();
-    });
+    // app.use(function(req, res, next) {
+    //     res.header(
+    //         "Access-Control-Allow-Headers",
+    //         "x-access-token, Origin, Content-Type, Accept"
+    //     );
+    //     next();
+    // });
 
     app.get("/api/assessment/all", [authJwt.verifyToken], AssessmentController.all);
     app.get("/api/assessment/one", [authJwt.verifyToken], AssessmentController.one);
