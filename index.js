4141d6fe59514475d4bd0471720d18d99c721284
BexHeider <95228424+BexHeider@users.noreply.github.com>
2022-08-01 20:17:44 -0500

    update url backend

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === "development") {
 }
 
 if (process.env.NODE_ENV === "production") {
- o = "https://fichasyprotocolosensalud.com"
+ o = "https://protocolosensalud.com"
 }
 
 var corsOptions = {
